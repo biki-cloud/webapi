01a9523271a8b6bcb29771124c7c1982f7163005
hibiki <hibiki123123@gmail.com>
2022-04-30 16:47:54 +0900

    Revert "some refactoring."
    This reverts commit f3da0e2a8665342c9d6c4d74076ab391a7f83c27.

--- deployment/k8s/website.sh
+++ deployment/k8s/website.sh
@@ -23,13 +23,13 @@ cp -r microservices/website/cmd/application/ui deployment/k8s/website-kube/websi
 cd deployment/k8s/website-kube
 
 echo "docker rmi 192.168.1.12:5010/website:v1.0.5"
-docker rmi 192.168.1.12:5010/website:v1.0.6
+docker rmi 192.168.1.12:5010/website:v1.0.5
 
 echo "docker build --no-cache -t 192.168.1.12:5010/website:v1.0.6 ."
-docker build --no-cache -t 192.168.1.12:5010/website:v1.0.8 .
+docker build --no-cache -t 192.168.1.12:5010/website:v1.0.7 .
 
 echo "docker push 192.168.1.12:5010/website:v1.0.6"
-docker push 192.168.1.12:5010/website:v1.0.8
+docker push 192.168.1.12:5010/website:v1.0.7
 
 # cd k8s
 cd ..
